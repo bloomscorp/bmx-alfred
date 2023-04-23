916aefa2471f1b7574a9d940f28b1a5b308eda7a
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-04-24 00:29:26 +0530

    release 0.0.2

--- src/main/java/com/bloomscorp/alfred/adapter/ILogBookDAO.java
+++ src/main/java/com/bloomscorp/alfred/adapter/ILogBookDAO.java
@@ -5,8 +5,6 @@ import com.bloomscorp.alfred.orm.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ILogBookDAO<A extends AuthenticationLog, L extends Log> {
-	JpaRepository<A, Long> getAuthenticationLogJpaRepository();
-	JpaRepository<L, Long> getLogJpaRepository();
 	long insertAuthenticationLog(A log);
 	long insertLog(L log);
 }
